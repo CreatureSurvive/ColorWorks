95c26b83292cd3e3db9a4e0d0b2959763ab7e7d2
CreatureSurvive <dbuehre@me.com>
2020-04-02 10:55:35 -0500

    Initial Commit

---
+++ ColorWorks/ColorWorks.h
@@ -0,0 +1,19 @@
+//
+//  ColorWorks.h
+//  ColorWorks
+//
+//  Created by Dana Buehre on 4/2/20.
+//  Copyright © 2020 CreatureCoding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ColorWorks.
+FOUNDATION_EXPORT double ColorWorksVersionNumber;
+
+//! Project version string for ColorWorks.
+FOUNDATION_EXPORT const unsigned char ColorWorksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ColorWorks/PublicHeader.h>
+
+
